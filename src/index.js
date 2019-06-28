62f96aabe219beebd7e468a0e531baba29b02a7d
Kasia Lach <lachkasia4@gmail.com>
2019-06-28 18:22:13 +0200

    Refactor styling

--- src/index.js
+++ src/index.js
@@ -3,17 +3,6 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import * as firebase from 'firebase'
-
-var config = {
-    apiKey: "AIzaSyAP3_3Q_MM0xcrGwf1yJS2mY-tln8-I-3A",
-    authDomain: "myapp-fe775.firebaseapp.com",
-    databaseURL: "https://myapp-fe775.firebaseio.com",
-    projectId: "myapp-fe775",
-    storageBucket: "myapp-fe775.appspot.com",
-    messagingSenderId: "877705586853"
-};
-firebase.initializeApp(config);
 
 
 ReactDOM.render(<App />, document.getElementById('root'));
